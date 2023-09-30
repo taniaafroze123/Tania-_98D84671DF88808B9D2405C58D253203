7f23e65dc31c4217189de79eb09d443db89b956f
taniaafroze123 <144889322+taniaafroze123@users.noreply.github.com>
2023-09-30 22:22:43 +0530

    Add files via upload

---
+++ challenge 3.1.py
@@ -0,0 +1,15 @@
+
+#challenge 3.1
+
+def linearSearchProduct(productList,targetProduct) :
+  indices = []
+  for index,product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+  
+  return indices
+products = ["shoes","boots","loafer","shoes","sandals","shoes"]
+target = "shoes"
+target2 = "apple"
+result = linearSearchProduct(products,target)
+print(result)
