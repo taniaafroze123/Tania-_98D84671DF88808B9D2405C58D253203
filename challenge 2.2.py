8cb6377bc048678bada13d9410c00361f0df028c
taniaafroze123 <144889322+taniaafroze123@users.noreply.github.com>
2023-09-30 22:21:38 +0530

    Add files via upload

---
+++ challenge 2.2.py
@@ -0,0 +1,17 @@
+#challenge 2.2
+
+
+class Player:
+  def play(self):
+    print("The player is playing cricket")
+
+class Batsman(Player):
+  def play(self):
+    print("The batsman is batting")
+
+class Bowler(Player):
+  def play(self):
+    print("The bowler is bowling")
+
+batsman=Batsman()
+bowler=Bowler()
